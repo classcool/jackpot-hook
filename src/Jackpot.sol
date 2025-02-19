ba406ef7520af28ceabdea45d87408d24b465b4f
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-02-19 12:54:57 -0600

    feat: 🐢 jackpot boilerplate and test setup

---
+++ src/Jackpot.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.24;
+
+import { IPoolManager } from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import { PoolKey } from "@uniswap/v4-core/src/types/PoolKey.sol";
+import { Hooks } from "v4-core/libraries/Hooks.sol";
+import { BalanceDelta } from "v4-core/types/BalanceDelta.sol";
+import { BeforeSwapDelta } from "v4-core/types/BeforeSwapDelta.sol";
+import { Currency } from "v4-core/types/Currency.sol";
+import { BaseHook } from "v4-periphery/src/utils/BaseHook.sol";
+
+contract Jackpot is BaseHook {
+
+    constructor(IPoolManager _manager) BaseHook(_manager) { }
+
+    error DynamicFeeNotSet(uint24 fee);
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: true,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: true,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function _beforeInitialize(address, PoolKey calldata key, uint160)
+        internal
+        pure
+        override
+        returns (bytes4)
+    {
+        // TODO:
+        // 1. Check that pool has a dynamic Fee
+        if (key.fee != 0x800000) revert DynamicFeeNotSet(key.fee);
+
+        // Otional: Choose if currency0 should always be ETH
+        // require(key.currency0 == Currency.wrap(address(0)), "Only ETH pools allowed.");
+
+        return this.beforeInitialize.selector;
+    }
+
+    struct JackpotLPParams {
+        address lpRewardee;
+        int24 tickLower;
+        int24 tikUpper;
+    }
+
+    function _afterAddLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata hookData
+    ) internal override returns (bytes4, BalanceDelta) {
+        // TODO:
+        // 1. check hookData struct for JackPot liquidity
+        JackpotLPParams memory data = abi.decode(hookData, (JackpotLPParams));
+        // if len(
+        return (this.afterAddLiquidity.selector, BalanceDelta.wrap(0));
+    }
+
+    function _beforeAddLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata hookData
+    ) internal override returns (bytes4) {
+        // TODO:
+        // Use struct for hookData
+
+        return this.beforeAddLiquidity.selector;
+    }
+
+    function _beforeRemoveLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal override returns (bytes4) {
+        // TODO:
+        return this.beforeRemoveLiquidity.selector;
+    }
+
+    function _beforeSwap(
+        address,
+        PoolKey calldata,
+        IPoolManager.SwapParams calldata,
+        bytes calldata
+    ) internal override returns (bytes4, BeforeSwapDelta, uint24) {
+        // TODO:
+        return (this.beforeSwap.selector, BeforeSwapDelta.wrap(0), 0);
+    }
+
+    function _afterSwap(
+        address,
+        PoolKey calldata,
+        IPoolManager.SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) internal override returns (bytes4, int128) {
+        // TODO:
+        return (this.afterSwap.selector, 0);
+    }
+
+}
---
+++ test/Jackpot.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.24;
+
+import { Jackpot } from "../src/Jackpot.sol";
+import { Deployers } from "@uniswap/v4-core/test/utils/Deployers.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { MockERC20 } from "solmate/src/test/utils/mocks/MockERC20.sol";
+import { Currency, CurrencyLibrary } from "v4-core/PoolManager.sol";
+import { PoolKey } from "v4-core/types/PoolKey.sol";
+
+import { Hooks } from "v4-core/libraries/Hooks.sol";
+import { LPFeeLibrary } from "v4-core/libraries/LPFeeLibrary.sol";
+import { TickMath } from "v4-core/libraries/TickMath.sol";
+
+contract JackpotTest is Deployers {
+
+    MockERC20 token;
+    Jackpot hook;
+
+    Currency token0;
+    Currency token1;
+
+    function setUp() public {
+        // deploy v4 core
+        deployFreshManagerAndRouters();
+
+        // deploy 2 currencies
+        (token0, token1) = deployMintAndApprove2Currencies();
+
+        // deploy our hook
+        uint160 flags = uint160(
+            Hooks.BEFORE_INITIALIZE_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.BEFORE_SWAP_FLAG
+                | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+        );
+        address hookAddress = address(flags);
+        deployCodeTo("Jackpot.sol", abi.encode(manager), hookAddress);
+        hook = Jackpot(hookAddress);
+
+        // initilize hook
+        (key,) =
+            initPool(token0, token1, hook, LPFeeLibrary.DYNAMIC_FEE_FLAG,SQRT_PRICE_1_1  );
+    }
+
+    function test_addLiquidity() public { 
+
+	}
+
+}
